9a08e737e64090f983ea6d293d3fabfbef5f7475
Louis Franceschi <louisbart3@gmail.com>
2024-01-07 14:50:39 +0100

    Sorted JS

---
+++ script.js
@@ -0,0 +1,52 @@
+let toggled = false;
+
+function playAudio(effect) {
+  const slide = new Audio("./assets/slide.mp3");
+  const click = new Audio("./assets/click.mp3");
+  switch (effect) {
+    case "slide":
+      slide.play();
+      break;
+    case "click":
+      click.play();
+      break;
+  }
+}
+
+function disableInput(condition) {
+  if (condition == true) {
+    document.getElementsByClassName("screen-input")[0].value = "";
+  } else {
+    document
+      .getElementsByClassName("screen-input")[0]
+      .focus({ focusVisible: true });
+    console.log("focusing");
+  }
+  document.getElementsByClassName("screen-input")[0].disabled = condition;
+}
+
+function buttonClick() {
+  playAudio("click");
+}
+
+function toggleSlider() {
+  playAudio("slide");
+  if (toggled === false) {
+    toggled = true;
+    document
+      .getElementsByClassName("slider-round")[0]
+      .classList.add("toggled-on");
+    disableInput(false);
+  } else {
+    toggled = false;
+    document
+      .getElementsByClassName("slider-round")[0]
+      .classList.remove("toggled-on");
+    disableInput(true);
+  }
+}
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => playAudio("click"));
+});
